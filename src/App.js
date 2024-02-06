0ffd30292d229e320594aea0f9beea45ffc31f85
Mik255 <mikael20informatica@gmail.com>
2024-02-06 17:04:02 -0300

    test4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
+const reload = () => window.location.reload();
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+      <Route path="/apple-app-site-association" onEnter={reload} />
+      <Route path="/.well-known/assetlinks.json" onEnter={reload} />
+    </Router>
   );
 }
 
